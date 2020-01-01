a233d79111c576e3764b7c7a0d9a933baa508d1c
Christoph Gohlke <cgohlke@uci.edu>
2019-12-31 18:22:39 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # fmkr/setup.py
 
 """Fmkr package setuptools script."""
@@ -12,18 +11,23 @@ with open('fmkr/fmkr.py') as fh:
     code = fh.read()
 
 version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
-description = re.search(r'"""(.*)\.[\r\n?|\n]', code).groups()[0]
-readme = re.search(r'[\r\n?|\n]{2}"""(.*)"""[\r\n?|\n]{2}__version__', code,
-                   re.MULTILINE | re.DOTALL).groups()[0]
-license = re.search(r'(# Copyright.*?[\r\n?|\n])[\r\n?|\n]+""', code,
+
+description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+                   code, re.MULTILINE | re.DOTALL).groups()[0]
+
+readme = '\n'.join([description, '=' * len(description)] +
+                   readme.splitlines()[1:])
+
+license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
                     re.MULTILINE | re.DOTALL).groups()[0]
 
-readme = '\n'.join([description, '=' * len(description)]
-                   + readme.splitlines()[1:])
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
     with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
     with open('README.rst', 'w') as fh:
         fh.write(readme)
@@ -38,7 +42,7 @@ setup(
     url='https://www.lfd.uci.edu/~gohlke/',
     packages=['fmkr'],
     install_requires=['lxml>=4.2'],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     license='BSD',
     platforms=['any'],
     classifiers=[
@@ -48,8 +52,8 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        ],
+        'Programming Language :: Python :: 3.8',
+    ],
 )
